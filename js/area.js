f116b2f8df34cbbde281444c74ab748137c21a96
Travor Liu <travor_lzh@outlook.com>
2018-07-28 16:09:54 +0800

    Barebones

---
+++ js/area.js
@@ -0,0 +1,14 @@
+/**
+ * @file	area.js
+ * @brief	Functions for calculating area of an triangle
+ */
+
+function area(base,height){
+	return base*height/2;
+}
+
+function area_heron(a,b,c){
+	var p=(a+b+c)/2;
+	var square=p*(p-a)*(p-b)*(p-c);
+	return Math.sqrt(square);
+}
